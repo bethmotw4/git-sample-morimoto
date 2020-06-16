938cc965f48835705980c5c073410c83e86d691c
森本 義基 <yoshiki.morimoto@rakus.internal>
2020-06-16 12:30:18 +0900

    Carのフィールドを追加

--- src/main/java/com/example/domain/Car.java
+++ src/main/java/com/example/domain/Car.java
@@ -3,6 +3,7 @@ package com.example.domain;
 public class Car {
 	private int speed;
 	private String color;
+	private Integer number;
 	public int getSpeed() {
 		return speed;
 	}
@@ -15,9 +16,16 @@ public class Car {
 	public void setColor(String color) {
 		this.color = color;
 	}
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
 	@Override
 	public String toString() {
-		return "Car [speed=" + speed + ", color=" + color + "]";
+		return "Car [speed=" + speed + ", color=" + color + ", number=" + number + "]";
 	}
+	
 	
 }
